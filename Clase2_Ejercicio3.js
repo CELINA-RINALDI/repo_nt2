df44177e6c00ea29eed6feeadb36b1d34ac8381d
CELINA RINALDI <44110736@comunidadort.edu.ar>
2022-08-29 22:44:06 -0300

    Clase 2- agrego ejercicio 3

---
+++ Clase2_Ejercicio3.js
@@ -0,0 +1,12 @@
+
+  const series = [
+    {nombre:"The Walking Dead", anioEstreno:2010},
+    {nombre:"Friends", anioEstreno:1994},
+    {nombre:"Stranger Things", anioEstreno:2016},
+    {nombre:"La casa de papel", anioEstreno:2017},
+    {nombre:"How I Met Your Mother", anioEstreno:2005 }
+]
+ 
+const copia = series.filter((serie) => serie.nombre != "Friends")
+console.log(copia)
+console.log(series)
